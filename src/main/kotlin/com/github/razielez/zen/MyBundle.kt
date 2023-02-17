89ee4a285285efd908d82621e2ad5abb754a1e6e
GitHub Action <action@github.com>
2023-02-17 08:29:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/razielez/zen/MyBundle.kt
+++ src/main/kotlin/com/github/razielez/zen/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.razielez.zen
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/razielez/zen/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/razielez/zen/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.razielez.zen.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.razielez.zen.MyBundle
 
 class MyApplicationService {
 
